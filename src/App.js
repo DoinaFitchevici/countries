fa04dda77707ec0e992c693ba5c5e3a4af133cfd
Doina Fitchevici <doinafitchevici@Doinas-MBP.home>
2023-10-11 18:49:39 -0400

    Added dropdown with country options and their flags

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [flag, setFlag] = useState("");
+  useEffect(() => {
+    fetch(
+      "https://gist.githubusercontent.com/pratikbutani/20ded7151103bb30737e2ab1b336eb02/raw/be1391e25487ded4179b5f1c8eedb81b01226216/country-flag.json"
+    )
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleChange = (event) => {
+    setFlag(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={handleChange}>
+        <option> Select Country</option>
+        {countries.map((country) => (
+          <option value={country.flag}>{country.country}</option>
+        ))}
+      </select>
+      <div>
+        <img src={flag} alt="flag" />
+      </div>
     </div>
   );
 }
